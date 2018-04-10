fb7bef93a7369f0264bb77b58969835feac7664c
ANTHONY <ANTHONY@DESKTOP-0RI6A0M>
2018-04-10 04:08:50 -0500

    Se subio los cambios de nombre

---
+++ AplicacionAngular4/SolucionProductosAngular.Repositorios/IProductoRepositorio.cs
@@ -0,0 +1,15 @@
+﻿
+using SolucionProductosAngular.AccesoDatos.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SolucionProductosAngular.Repositorios
+{
+     public  interface IProductoRepositorio
+    {
+        Task<List<Productos>> ObtenerTotalProductos();
+        Task<bool> GuardarProducto(Productos producto);
+        Task<bool> EliminarProductoPorIDAsync(int id);
+         Productos ObtenerProductosPorId(int id);
+    }
+}
